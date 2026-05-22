0c11a021d9b78d8a144c07f45a4a4aea973bcc7a
Muhammed Jamzeeth <mrjamzee002@gmail.com>
2026-05-22 18:07:44 +0530

    Initial commit

---
+++ movieapp/src/main/java/com/movie_catalog/movieapp/MovieappApplication.java
@@ -0,0 +1,13 @@
+package com.movie_catalog.movieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieappApplication.class, args);
+	}
+
+}
